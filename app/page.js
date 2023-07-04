ef75e5ea62bd033edf257b1e8fb9d15931280b65
성해호 <hhseong@tidesquare.com>
2023-07-04 16:18:29 +0900

    + mongoDB

--- app/page.js
+++ app/page.js
@@ -1,8 +1,15 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import { connectDB } from "@/util/database"
 
-export default function Home() {
+export default async function Home() {
+
+  const client = await connectDB
+  const db = client.db('forum')
+  let result = await db.collection('post').find().toArray()
+  console.log(result)
+
   return (
-   <div></div>
+   <div>
+    <h4>gd</h4>
+   </div>
   )
 }
